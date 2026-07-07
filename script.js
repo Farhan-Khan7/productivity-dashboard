b80c5cdf30b1058e002d6049be5e0615baf62021
Farhan-Khan7 <fk5518744@gmail.com>
2026-07-07 18:56:43 +0530

    I have implement theme change functionalty  based on time

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ themeChange.addEventListener("change", function () {
   console.log(savedTheme);
 
   if (savedTheme) {
-    body.setAttribute("data-theme", savedTheme);
+    body.setAttribute("theme", savedTheme);
     themeVideo.setAttribute("src", `assets/${savedTheme}-wheather-video.mp4`);
     themeChange.value = savedTheme;
   }
@@ -40,26 +40,79 @@ themeChange.addEventListener("change", function () {
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
-    body.setAttribute("data-theme", savedTheme);
-    themeVideo.setAttribute("src", `assets/${savedTheme}-wheather-video.mp4`);
-    themeChange.value = savedTheme;
+  body.setAttribute("theme", savedTheme);
+  themeVideo.setAttribute("src", `assets/${savedTheme}-wheather-video.mp4`);
+  themeChange.value = savedTheme;
   greetings();
 }
 // Theme Changing JS Done
 
-
 const themeToggleBtn = document.querySelector("#theme-toggle-card");
 
-themeToggleBtn.addEventListener("click", function(){
-    body.setAttribute("data-theme" ,"night");
-    themeVideo.setAttribute("src", `assets/nightu-wheather-video.mp4`)
-})
+themeToggleBtn.addEventListener("click", function () {
+  body.setAttribute("data-theme", "night");
+  themeVideo.setAttribute("src", `assets/night-wheather-video.mp4`);
+  themeChange.value = savedTheme;
+});
 
+// toggle done
 
+const currentDate = document.querySelector("#current-date");
+const currentTime = document.querySelector("#current-time");
 
+function updateDateTime() {
+  const now = new Date();
+  const date = now.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+    year: "numeric",
+  });
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
+  currentDate.textContent = date;
+  currentTime.textContent = time;
+}
 
+function updateThemeBasedOnTime() {
+  const hour = new Date().getHours();
+  // morning
 
+  if (hour >= 6 && hour < 12) {
+    body.setAttribute("theme", "morning");
+    themeVideo.src = "assets/morning-wheather-video.mp4";
+    themeVideo.load();
+    themeVideo.play();
+    themeChange.value = "";
+  } else if (hour >= 12 && hour < 18) {
+    body.setAttribute("theme", "day");
+    themeVideo.src = "assets/day-wheather-video.mp4";
+    themeVideo.load();
+    themeVideo.play();
+    themeChange.value = "";
+  } else if (hour >= 18 && hour < 20) {
+    body.setAttribute("theme", "evening");
+    themeVideo.src = "assets/evening-wheather-video.mp4";
+    themeVideo.load();
+    themeVideo.play();
+    themeChange.value = "";
+  } else {
+    body.setAttribute("theme", "night");
+    themeVideo.src = "assets/night-wheather-video.mp4";
+    themeVideo.load();
+    themeVideo.play();
+    themeChange.value = "";
+  }
+}
+
+updateDateTime();
+updateThemeBasedOnTime();
+setInterval(updateThemeBasedOnTime, 60000);
+setInterval(updateDateTime, 1000);
 
 const quotes = [
   {
