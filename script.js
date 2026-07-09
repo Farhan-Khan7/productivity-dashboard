5144cda694c76bee736834c7c5e4641f13fe4eec
Farhan-Khan7 <fk5518744@gmail.com>
2026-07-10 03:46:35 +0530

    I have fixed theme buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
-const themeChange = document.querySelector("#theme-select");
-const themeOptions = document.querySelector("#theme-select option");
+const themeBtns = document.querySelectorAll("#theme-btns .theme-select");
+// const themeChange = document.querySelectorAll(".theme-select");
+
 const themeVideo = document.querySelector("#theme-video");
 
 const greeting = document.querySelector("#greeting-name");
@@ -22,40 +23,13 @@ function greetings() {
 greetings();
 // greetings functions
 
-themeChange.addEventListener("change", function () {
-  localStorage.setItem("theme", this.value);
-
-  const savedTheme = localStorage.getItem("theme");
-
-  console.log(savedTheme);
-
-  if (savedTheme) {
-    body.setAttribute("theme", savedTheme);
-    themeVideo.setAttribute("src", `assets/${savedTheme}-wheather-video.mp4`);
-    themeChange.value = savedTheme;
-  }
-  greetings();
+themeBtns.forEach((buttons) => {
+  buttons.addEventListener("click", function (details) {
+    console.log(buttons.dataset.theme);
+  });
 });
 
-const savedTheme = localStorage.getItem("theme");
-
-if (savedTheme) {
-  body.setAttribute("theme", savedTheme);
-  themeVideo.setAttribute("src", `assets/${savedTheme}-wheather-video.mp4`);
-  themeChange.value = savedTheme;
-  greetings();
-}
-// Theme Changing JS Done
-
-const themeToggleBtn = document.querySelector("#theme-toggle-card");
-
-themeToggleBtn.addEventListener("click", function () {
-  body.setAttribute("data-theme", "night");
-  themeVideo.setAttribute("src", `assets/night-wheather-video.mp4`);
-  themeChange.value = savedTheme;
-});
-
-// toggle done
+//
 
 const currentDate = document.querySelector("#current-date");
 const currentTime = document.querySelector("#current-time");
@@ -78,35 +52,35 @@ function updateDateTime() {
   currentTime.textContent = time;
 }
 
+let currentTheme = "";
 function updateThemeBasedOnTime() {
   const hour = new Date().getHours();
-  // morning
+
+  let newThaeme = "";
 
   if (hour >= 6 && hour < 12) {
-    body.setAttribute("theme", "morning");
-    themeVideo.src = "assets/morning-wheather-video.mp4";
-    themeVideo.load();
-    themeVideo.play();
-    themeChange.value = "";
+    newTheme = "morning";
   } else if (hour >= 12 && hour < 18) {
-    body.setAttribute("theme", "day");
-    themeVideo.src = "assets/day-wheather-video.mp4";
-    themeVideo.load();
-    themeVideo.play();
-    themeChange.value = "";
+    newTheme = "day";
   } else if (hour >= 18 && hour < 20) {
-    body.setAttribute("theme", "evening");
-    themeVideo.src = "assets/evening-wheather-video.mp4";
-    themeVideo.load();
-    themeVideo.play();
-    themeChange.value = "";
+    newTheme = "evening";
   } else {
-    body.setAttribute("theme", "night");
-    themeVideo.src = "assets/night-wheather-video.mp4";
-    themeVideo.load();
-    themeVideo.play();
-    themeChange.value = "";
+    newTheme = "night";
   }
+
+  // Agar theme already wahi hai to kuch mat karo
+
+  if (currentTheme === newTheme) return;
+
+  currentTheme = newTheme;
+
+  body.setAttribute("theme", newTheme);
+
+  themeVideo.src = `assets/${newTheme}-wheather-video.mp4`;
+
+  themeVideo.load();
+
+  themeVideo.play();
 }
 
 updateDateTime();
@@ -114,103 +88,115 @@ updateThemeBasedOnTime();
 setInterval(updateThemeBasedOnTime, 60000);
 setInterval(updateDateTime, 1000);
 
-const quotes = [
-  {
-    quote: "Success is built in silence long before it is seen in public.",
-    author: "— Naval Ravikant",
-  },
-  {
-    quote:
-      "The person who can stay focused while others are distracted will own the future.",
-    author: "— James Clear",
-  },
-  {
-    quote:
-      "Discipline is choosing between what you want now and what you want most.",
-    author: "— Abraham Lincoln",
-  },
-  {
-    quote: "Your comfort zone is the most expensive place to live.",
-    author: "— Alex Hormozi",
-  },
-  {
-    quote: "Small daily improvements create extraordinary results over time.",
-    author: "— Robin Sharma",
-  },
-  {
-    quote:
-      "The hardest worker in the room eventually becomes the most valuable.",
-    author: "— Patrick Bet-David",
-  },
-  {
-    quote: "Talent opens the door, consistency keeps it open.",
-    author: "— Angela Duckworth",
-  },
-  {
-    quote: "The future rewards those who can delay gratification today.",
-    author: "— Morgan Housel",
-  },
-  {
-    quote: "Most people quit when they are one breakthrough away from success.",
-    author: "— Les Brown",
-  },
-  {
-    quote: "Your habits are the architects of your destiny.",
-    author: "— James Clear",
-  },
-  {
-    quote: "Dreams become reality only when action becomes a daily ritual.",
-    author: "— Denzel Washington",
-  },
-  {
-    quote: "You do not rise to your goals, you fall to your systems.",
-    author: "— James Clear",
-  },
-  {
-    quote: "Every expert was once a beginner who refused to quit.",
-    author: "— Helen Hayes",
-  },
-  {
-    quote: "A year from now you will wish you had started today.",
-    author: "— Karen Lamb",
-  },
-  {
-    quote: "Focus is a superpower in a world designed to distract you.",
-    author: "— Cal Newport",
-  },
-  {
-    quote: "Your current situation is not your final destination.",
-    author: "— Nido Qubein",
-  },
-  {
-    quote: "What feels impossible today becomes your warm-up tomorrow.",
-    author: "— David Goggins",
-  },
-  {
-    quote:
-      "The quality of your future depends on what you repeatedly do today.",
-    author: "— Brian Tracy",
-  },
-  {
-    quote: "My Internal Wealth Will Definitely Create My External Wealth...",
-    author: "— Farhan Khan",
-  },
-];
+// complete live date and time code
+
+// const quotes = [
+//   {
+//     quote: "Success is built in silence long before it is seen in public.",
+//     author: "— Naval Ravikant",
+//   },
+//   {
+//     quote:
+//       "The person who can stay focused while others are distracted will own the future.",
+//     author: "— James Clear",
+//   },
+//   {
+//     quote:
+//       "Discipline is choosing between what you want now and what you want most.",
+//     author: "— Abraham Lincoln",
+//   },
+//   {
+//     quote: "Your comfort zone is the most expensive place to live.",
+//     author: "— Alex Hormozi",
+//   },
+//   {
+//     quote: "Small daily improvements create extraordinary results over time.",
+//     author: "— Robin Sharma",
+//   },
+//   {
+//     quote:
+//       "The hardest worker in the room eventually becomes the most valuable.",
+//     author: "— Patrick Bet-David",
+//   },
+//   {
+//     quote: "Talent opens the door, consistency keeps it open.",
+//     author: "— Angela Duckworth",
+//   },
+//   {
+//     quote: "The future rewards those who can delay gratification today.",
+//     author: "— Morgan Housel",
+//   },
+//   {
+//     quote: "Most people quit when they are one breakthrough away from success.",
+//     author: "— Les Brown",
+//   },
+//   {
+//     quote: "Your habits are the architects of your destiny.",
+//     author: "— James Clear",
+//   },
+//   {
+//     quote: "Dreams become reality only when action becomes a daily ritual.",
+//     author: "— Denzel Washington",
+//   },
+//   {
+//     quote: "You do not rise to your goals, you fall to your systems.",
+//     author: "— James Clear",
+//   },
+//   {
+//     quote: "Every expert was once a beginner who refused to quit.",
+//     author: "— Helen Hayes",
+//   },
+//   {
+//     quote: "A year from now you will wish you had started today.",
+//     author: "— Karen Lamb",
+//   },
+//   {
+//     quote: "Focus is a superpower in a world designed to distract you.",
+//     author: "— Cal Newport",
+//   },
+//   {
+//     quote: "Your current situation is not your final destination.",
+//     author: "— Nido Qubein",
+//   },
+//   {
+//     quote: "What feels impossible today becomes your warm-up tomorrow.",
+//     author: "— David Goggins",
+//   },
+//   {
+//     quote:
+//       "The quality of your future depends on what you repeatedly do today.",
+//     author: "— Brian Tracy",
+//   },
+//   {
+//     quote: "My Internal Wealth Will Definitely Create My External Wealth...",
+//     author: "— Farhan Khan",
+//   },
+// ];
+
+// function updateQuote() {
+//   let randomQuoteIdx = Math.floor(Math.random() * quotes.length);
+
+//   quoteText.textContent = quotes[randomQuoteIdx].quote;
+//   quoteAuthor.textContent = quotes[randomQuoteIdx].author;
+// }
+// updateQuote();
+
+// Quote Chaning JS Done
 
 const quoteText = document.querySelector("#quote-text");
 const quoteAuthor = document.querySelector("#quote-author");
 const quoteRefreshBtn = document.querySelector("#new-quote-btn");
+async function updateQuote() {
+  const response = await fetch("https://dummyjson.com/quotes/random");
 
-function updateQuote() {
-  let randomQuoteIdx = Math.floor(Math.random() * quotes.length);
+  const data = await response.json();
 
-  quoteText.textContent = quotes[randomQuoteIdx].quote;
-  quoteAuthor.textContent = quotes[randomQuoteIdx].author;
+  quoteText.textContent = data.quote;
+
+  quoteAuthor.textContent = data.author;
 }
 updateQuote();
 quoteRefreshBtn.addEventListener("click", updateQuote);
-
-// Quote Chaning JS Done
 
 // Weather Location
 const weatherLocation = document.querySelector("#weather-location");
@@ -244,7 +230,7 @@ navigator.geolocation.getCurrentPosition((position) => {
   let lon = position.coords.longitude;
 });
 
-async function getWeather(lat , lon) {
+async function getWeather(lat, lon) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=90e75288217a53e1631469e446f3fbb9&units=metric`,
   );
@@ -283,50 +269,38 @@ async function getWeather(lat , lon) {
 }
 
 function getLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
 
-    navigator.geolocation.getCurrentPosition((position) => {
+    const lon = position.coords.longitude;
 
-        const lat = position.coords.latitude;
-
-        const lon = position.coords.longitude;
-
-        getWeather(lat, lon);
-
-    });
-
+    getWeather(lat, lon);
+  });
 }
 
 getLocation();
 
 weatherRefreshBtn.addEventListener("click", getLocation);
 
-
 const dashboard = document.querySelector("#dashboard-main");
 const todos = document.querySelector("#todo-main");
 
-
 const navTabs = document.querySelectorAll(".nav-menu .nav-item");
 
-
-navTabs.forEach((link) =>{
-  link.addEventListener("click", function(){
-    navTabs.forEach((item) =>{
-      item.classList.remove("active")
+navTabs.forEach((link) => {
+  link.addEventListener("click", function () {
+    navTabs.forEach((item) => {
+      item.classList.remove("active");
     });
 
     link.classList.add("active");
 
-
-    if(link.dataset.page === "dashboard"){
-        dashboard.style.display = "flex";
-        todos.style.display = "none"
-      }else if(link.dataset.page === "todo-list"){
-        dashboard.style.display = "none"
-        todos.style.display = "initial";
-
-      }
-
-  })
-})
-
-
+    if (link.dataset.page === "dashboard") {
+      dashboard.style.display = "flex";
+      todos.style.display = "none";
+    } else if (link.dataset.page === "todo-list") {
+      dashboard.style.display = "none";
+      todos.style.display = "initial";
+    }
+  });
+});
